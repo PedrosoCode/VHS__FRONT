4ffb9e672dbb9df80133a776e2ac39e19095a095
gabriel <gb.ribeiropedroso@gmail.com>
2026-03-29 11:44:59 -0300

    Componente Navbar

---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { shallowRef } from 'vue'
+
+const bMenuAberto = shallowRef(true)
+</script>
+
+<template>
+
+<v-app-bar>
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon @click="bMenuAberto = !bMenuAberto"></v-app-bar-nav-icon>
+  </template>
+
+  <v-app-bar-title>Application Bar</v-app-bar-title>
+</v-app-bar>
+
+<v-navigation-drawer :width="400" v-model="bMenuAberto">
+  <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+  <v-divider></v-divider>
+  <v-list-item link title="List Item 1"></v-list-item>
+  <v-list-item link title="List Item 2"></v-list-item>
+  <v-list-item link title="List Item 3"></v-list-item>
+</v-navigation-drawer>
+
+
+</template>
---
+++ src/views/CentralChamadosView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MainNavbar from '@/components/MainNavbar.vue'
+</script>
+
+<template>
+    <MainNavbar />
+
+    <v-main>
+      <div>central</div>
+    </v-main>
+</template>
